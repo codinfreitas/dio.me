1640607970779b05140de96d8edd241568e0732a
codinfreitas <gabriel.sistemas.00@gmail.com>
2023-10-20 11:09:46 -0300

    Projeto Dio

---
+++ src/5_project/5_project.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	c := 100
+	k := c + 273
+
+	fmt.Printf("A temperatura de ebulição da água em graus celsius é: %v°C, mas em graus kelvin a temperatura é: %vK   ", c, k)
+}
